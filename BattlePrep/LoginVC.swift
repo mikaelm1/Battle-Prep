491c8e2eef100318e015cfb7b5cb075bf0498dfd
Mikael Mukhsikaroyan <mikaelm3981@gmail.com>
2016-04-21 10:27:09 -0700

    feat: Created UI for login page and connected outlets and actions

---
+++ BattlePrep/LoginVC.swift
@@ -0,0 +1,53 @@
+//
+//  LoginVC.swift
+//  BattlePrep
+//
+//  Created by Mikael Mukhsikaroyan on 4/21/16.
+//  Copyright © 2016 MSquared. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var signInButton: UIButton!
+    @IBOutlet weak var facebookButton: UIButton!
+    @IBOutlet weak var twitterButton: UIButton!
+    @IBOutlet weak var signUpButton: UIButton!
+    @IBOutlet weak var signUpLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    override func shouldAutorotate() -> Bool {
+        return false
+    }
+    
+    override func supportedInterfaceOrientations() -> UIInterfaceOrientationMask {
+        return .Portrait
+    }
+
+    @IBAction func signInButtonPressed(sender: UIButton) {
+    }
+    
+    @IBAction func facebookLoginPressed(sender: UIButton) {
+        
+    }
+    
+    @IBAction func twitterLoginPressed(sender: UIButton) {
+        
+    }
+    
+    @IBAction func signUpPressed(sender: UIButton) {
+        
+    }
+
+}
+
+
+
+
