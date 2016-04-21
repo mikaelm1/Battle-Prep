094af9ee6875e63968bd4080728410ea798da905
Mikael Mukhsikaroyan <mikaelm3981@gmail.com>
2016-04-21 10:36:38 -0700

    feat: Created UI for new account screen and connected outlets and actions.

---
+++ BattlePrep/NewAccountVC.swift
@@ -0,0 +1,31 @@
+//
+//  NewAccountVC.swift
+//  BattlePrep
+//
+//  Created by Mikael Mukhsikaroyan on 4/21/16.
+//  Copyright © 2016 MSquared. All rights reserved.
+//
+
+import UIKit
+
+class NewAccountVC: UIViewController {
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func createButtonPressed(sender: UIButton) {
+        
+    }
+
+
+}
+
+
+
+
